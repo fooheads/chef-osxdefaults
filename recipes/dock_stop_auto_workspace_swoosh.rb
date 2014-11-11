47edcac6aaac504154b6166aed344587d138ca79
Jon-Erling Dahl <joda@soloduo.se>
2014-11-11 12:00:01 +0100

    Added setting for disabling auto swoosh between spaces

---
+++ recipes/dock_stop_auto_workspace_swoosh.rb
@@ -0,0 +1,5 @@
+osxdefaults_defaults "Do not switch to a space with open windows for the application when switching to it" do
+  domain 'com.apple.dock'
+  key 'workspaces-auto-swoosh'
+  boolean false
+end

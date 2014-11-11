fdeb863e7684856190d63e9b934a19ca03fe7c3f
Jon-Erling Dahl <joda@soloduo.se>
2014-11-11 12:17:01 +0100

    Reload dock after turning off swooshing

--- recipes/dock_stop_auto_workspace_swoosh.rb
+++ recipes/dock_stop_auto_workspace_swoosh.rb
@@ -1,5 +1,11 @@
 osxdefaults_defaults "Do not switch to a space with open windows for the application when switching to it" do
   domain 'com.apple.dock'
   key 'workspaces-auto-swoosh'
-  boolean false
+  boolean true
 end
+
+execute "reload dock" do
+  command "killall Dock"
+end
+
+
